1b960401815db0d9e0bf87a7a4aee42688762850
Quangsumi <djquangsumi@gmail.com>
2020-07-13 15:26:47 +0700

    Implemented a sample service (ProductCategoryService)

---
+++ TeduShop.Service/IProductCategoryService.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Service
+{
+    public interface IProductCategoryService
+    {
+        ProductCategory Add(ProductCategory ProductCategory);
+
+        void Update(ProductCategory ProductCategory);
+
+        ProductCategory Delete(int id);
+
+        IEnumerable<ProductCategory> GetAll();
+
+        IEnumerable<ProductCategory> GetAll(string keyword);
+
+        IEnumerable<ProductCategory> GetAllByParentId(int parentId);
+
+        ProductCategory GetById(int id);
+
+        void Save();
+    }
+}
