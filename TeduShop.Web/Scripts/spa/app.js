6075dfa227277a7cd08cdb64bc9db22f589ad945
Quangsumi <djquangsumi@gmail.com>
2020-07-14 16:23:41 +0700

    Installed bower from NuGet, Installed angular from bower, first experience with angular

---
+++ TeduShop.Web/Scripts/spa/app.js
@@ -0,0 +1,15 @@
+﻿/// <reference path="../../bower_components/angular/angular.js" />
+
+var myApp = angular.module('myModule', []);
+
+myController.$inject = ['$scope'];
+
+myApp.controller('myController', myController);
+
+myApp.controller('mySecondController', function ($scope) {
+    $scope.message = "This message comes from mySecondController";
+});
+
+function myController($scope) {
+    $scope.message = "This message comes from myController";
+}
