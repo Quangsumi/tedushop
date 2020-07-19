b12b8e30386c99772270b967bf164e7ea78e920e
DESKTOP-DAL6344\Quangsumi <djquangsumi@gmail.com>
2020-07-19 17:47:35 +0700

    Paging for ProductCategoryList using Angular at the front-end and API at the back-end

--- TeduShop.Web/Api/ProductCategoryController.cs
+++ TeduShop.Web/Api/ProductCategoryController.cs
@@ -7,6 +7,7 @@ using System.Net.Http;
 using System.Web.Http;
 using TeduShop.Model.Models;
 using TeduShop.Service;
+using TeduShop.Web.Helper;
 using TeduShop.Web.ViewModels;
 
 namespace TeduShop.Web.Api
@@ -23,17 +24,31 @@ namespace TeduShop.Web.Api
         }
 
         [Route("getall")]
-        public HttpResponseMessage GetAll(HttpRequestMessage request)
+        public HttpResponseMessage GetAll(HttpRequestMessage request, int page, int pageSize = 20)
         {
             return CreateHttpResponse(request, () =>
             {
+                int totalRow = 0;
+
                 var model = _productCategoryService.GetAll();
+                
+                totalRow = model.Count();
+
+                var query = model.OrderByDescending(m => m.CreatedBy).Skip(pageSize * page).Take(pageSize);
 
                 var responseData = new List<ProductCategoryViewModel>();
                 
-                AutoMapperConfig.Mapper.Map(model, responseData);
+                AutoMapperConfig.Mapper.Map(query, responseData);
 
-                var response = request.CreateResponse(HttpStatusCode.OK, responseData);
+                var paginationList = new PaginationSet<ProductCategoryViewModel>
+                {
+                    Items = responseData,
+                    Page = page,
+                    TotalCount = totalRow,
+                    TotalPages = (int)Math.Ceiling((decimal)totalRow / pageSize)
+                };
+
+                var response = request.CreateResponse(HttpStatusCode.OK, paginationList);
                 
                 return response; 
             });
