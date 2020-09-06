1068be7268f5973041bca13c233db1e2ce95df70
DESKTOP-DAL6344\Quangsumi <djquangsumi@gmail.com>
2020-09-07 05:06:10 +0700

    Added CKEditor for adding product view

---
+++ TeduShop.Service/IProductService.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Service
+{
+    public interface IProductService
+    {
+        Product Add(Product Product);
+
+        void Update(Product Product);
+
+        Product Delete(int id);
+
+        IEnumerable<Product> GetAll();
+
+        IEnumerable<Product> GetAll(string keyword);
+
+        Product GetById(int id);
+
+        void Save();
+    }
+}
---
+++ TeduShop.Web/Api/ProductController.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Script.Serialization;
+using TeduShop.Model.Models;
+using TeduShop.Service;
+using TeduShop.Web.Helper;
+using TeduShop.Web.ViewModels;
+
+namespace TeduShop.Web.Api
+{
+    [RoutePrefix("api/product")]
+    public class ProductController : ApiBaseController
+    {
+        private IProductService _productService;
+
+        public ProductController(IErrorService errorService, IProductService productService) : base(errorService)
+        {
+            this._productService = productService;
+        }
+
+        [Route("getallparents")]
+        [HttpGet]
+        public HttpResponseMessage GetAll(HttpRequestMessage request)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                var model = _productService.GetAll();
+
+                var listProductVm = new List<ProductViewModel>();
+
+                var responseData = AutoMapperConfig.Mapper.Map(model, listProductVm);
+
+                var response = request.CreateResponse(HttpStatusCode.OK, responseData);
+
+                return response;
+            });
+        }
+
+        [Route("getbyid/{id:int}")]
+        [HttpGet]
+        public HttpResponseMessage GetById(HttpRequestMessage request, int id)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                var model = _productService.GetById(id);
+
+                var productVm = new ProductViewModel();
+
+                var responseData = AutoMapperConfig.Mapper.Map(model, productVm);
+
+                var response = request.CreateResponse(HttpStatusCode.OK, responseData);
+
+                return response;
+            });
+        }
+
+        [Route("getall")]
+        [HttpGet]
+        public HttpResponseMessage GetAll(HttpRequestMessage request, string keyword, int page, int pageSize = 20)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                int totalRow = 0;
+                var model = _productService.GetAll(keyword);
+
+                totalRow = model.Count();
+                var query = model.OrderByDescending(x => x.CreatedDate).Skip(page * pageSize).Take(pageSize);
+
+                var listProductVm = new List<ProductViewModel>();
+
+                var responseData = AutoMapperConfig.Mapper.Map(query, listProductVm);
+
+                var paginationSet = new PaginationSet<ProductViewModel>()
+                {
+                    Items = responseData,
+                    Page = page,
+                    TotalCount = totalRow,
+                    TotalPages = (int)Math.Ceiling((decimal)totalRow / pageSize)
+                };
+                var response = request.CreateResponse(HttpStatusCode.OK, paginationSet);
+                return response;
+            });
+        }
+
+
+        [Route("create")]
+        [HttpPost]
+        [AllowAnonymous]
+        public HttpResponseMessage Create(HttpRequestMessage request, ProductViewModel productCategoryVm)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (!ModelState.IsValid)
+                {
+                    response = request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    var newProduct = new Product();
+                    AutoMapperConfig.Mapper.Map(productCategoryVm, newProduct);
+                    
+                    newProduct.CreatedDate = DateTime.Now;
+                    
+                    _productService.Add(newProduct);
+                    _productService.Save();
+
+                    var responseData = AutoMapperConfig.Mapper.Map(newProduct, productCategoryVm);
+
+                    response = request.CreateResponse(HttpStatusCode.Created, responseData);
+                }
+
+                return response;
+            });
+        }
+
+        [Route("update")]
+        [HttpPut]
+        [AllowAnonymous]
+        public HttpResponseMessage Update(HttpRequestMessage request, ProductViewModel productVm)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (!ModelState.IsValid)
+                {
+                    response = request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    var dbProduct = _productService.GetById(productVm.Id);
+
+                    AutoMapperConfig.Mapper.Map(productVm, dbProduct);
+
+                    dbProduct.UpdatedDate = DateTime.Now;
+
+                    _productService.Update(dbProduct);
+                    _productService.Save();
+
+                    var responseData = AutoMapperConfig.Mapper.Map(dbProduct, productVm);
+                    response = request.CreateResponse(HttpStatusCode.Created, responseData);
+                }
+
+                return response;
+            });
+        }
+
+        [Route("delete")]
+        [HttpDelete]
+        [AllowAnonymous]
+        public HttpResponseMessage Delete(HttpRequestMessage request, int id)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (!ModelState.IsValid)
+                {
+                    response = request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    var oldProduct = _productService.Delete(id);
+                    _productService.Save();
+
+                    var oldProductVm = new ProductViewModel();
+
+                    var responseData = AutoMapperConfig.Mapper.Map(oldProduct, oldProductVm);
+                    response = request.CreateResponse(HttpStatusCode.Created, responseData);
+                }
+
+                return response;
+            });
+        }
+        [Route("deletemulti")]
+        [HttpDelete]
+        [AllowAnonymous]
+        public HttpResponseMessage DeleteMulti(HttpRequestMessage request, string checkedProducts)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (!ModelState.IsValid)
+                {
+                    response = request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    var listProductCategory = new JavaScriptSerializer().Deserialize<List<int>>(checkedProducts);
+                    foreach (var item in listProductCategory)
+                    {
+                        _productService.Delete(item);
+                    }
+
+                    _productService.Save();
+
+                    response = request.CreateResponse(HttpStatusCode.OK, listProductCategory.Count);
+                }
+
+                return response;
+            });
+        }
+    }
+}
